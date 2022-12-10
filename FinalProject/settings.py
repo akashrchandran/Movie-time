e7588d9241f0f554ee0d29e386adc50181fc4e65
Akash R Chandran <chandranrakash@gmail.com>
2022-12-10 23:14:28 +0530

    updated

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -132,6 +132,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 MEDIA_URL = '/media/'
 
