daf62c85a0ec40fbfcf4830aefd1ef96121e3cb7
Akash R Chandran <chandranrakash@gmail.com>
2022-12-10 23:41:18 +0530

    updated

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', default='ejdnj3e32y4732newf34u9f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
