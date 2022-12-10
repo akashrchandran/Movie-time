ef5bc17e53520e68c2542f3de02fa98c616ca1d7
Akash R Chandran <chandranrakash@gmail.com>
2022-12-10 22:29:48 +0530

    updated

--- manage.py
+++ manage.py
@@ -14,9 +14,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
     current_path = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(os.path.join(current_path, "FinalProject"))
     application = get_wsgi_application()
+    execute_from_command_line(sys.argv)
 if __name__ == '__main__':
     main()
