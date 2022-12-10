6c065aca739fce789a269d6a74313debf5aa5b64
Akash R Chandran <chandranrakash@gmail.com>
2022-12-10 22:19:26 +0530

    updated

--- manage.py
+++ manage.py
@@ -15,7 +15,8 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, "FinalProject"))
 
 if __name__ == '__main__':
     main()
