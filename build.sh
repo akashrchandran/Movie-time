657a5d03e3156e24969978365292686123adbb68
Akash R Chandran <chandranrakash@gmail.com>
2023-03-14 23:14:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 mkdir static
-python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
+python manage.py collectstatic
 gunicorn FinalProject.wsgi
