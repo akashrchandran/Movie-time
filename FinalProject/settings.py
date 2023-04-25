ac04139593a187faa2a48311aaca2e0c69714599
Akash R Chandran <chandranrakash@gmail.com>
2023-04-25 23:41:13 +0530

    Update settings.py

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', default='ejdnj3e32y4732newf34u9f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["movieztime.tk","127.0.0.1:8000"]
 CSRF_TRUSTED_ORIGINS = ['https://*.movieztime.tk']
