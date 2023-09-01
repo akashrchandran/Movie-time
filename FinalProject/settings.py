f9f7c9dd65695e6cae51f2a90c82fbd43d8c8478
Akash R Chandran <chandranrakash@gmail.com>
2023-09-02 00:27:14 +0530

    updated

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -126,12 +126,10 @@ DATE_FORMAT = "d M, Y"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 
